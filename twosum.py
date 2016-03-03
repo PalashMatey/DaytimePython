af77ef6024b33ef9620fed9dbefe2c06ea9195fb
PalashMatey <pm2824@columbia.edu>
2016-03-03 18:06:20 -0500

    Adding stuff

--- twosum.py
+++ twosum.py
@@ -1,11 +1,12 @@
 def twoSum(nums, target):
     dic = {}
     for i, n in enumerate(nums):
-	print i,n,nums
-	print dic 
+	print i,n
+	  
         if n in dic:
             return [dic[n], i]
         dic[target-n] = i
+	print dic
 nums = [1,2,3,4,5,6,7]
 target = 13
 hel = twoSum(nums,target)
