d60a4207e70b4a38bf8755043858ee3017a19469
PalashMatey <pm2824@columbia.edu>
2016-03-22 15:00:22 -0400

    Basic Queue

--- MITEdxPython/queue.py
+++ MITEdxPython/queue.py
@@ -3,9 +3,17 @@ class Queue(object):
 		self.vals = []
 		self.front = 0
 		self.rear = 0
-	def __str__(self):
-		return self.vals
 	def insert(self,e):
 		self.vals.append(e)
-		self.front = self.front +1
-		self.front = self.rear
+		self.rear = self.rear +1
+	def remove(self):
+		if len(self.vals) == 0:
+			print 'Queue is empty'
+		else:
+			return self.vals.pop(0)
+			self.front = self.front + 1
+			try:
+				if self.front > self.rear:
+					print 'Queue is empty'
+			except e:
+				print e
