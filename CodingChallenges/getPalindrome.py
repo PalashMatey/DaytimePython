92d89e9b46e5c42fdd967d8109a57325792ac969
PalashMatey <pm2824@columbia.edu>
2016-11-02 21:59:20 -0400

    Palindrome
    Found a faster way

--- CodingChallenges/getPalindrome.py
+++ CodingChallenges/getPalindrome.py
@@ -1,18 +1,8 @@
 def isPalindrome(s):
-    valid = []
-    for character in s:
-        if character.isalpha():
-            valid.append(character)
-        else:
-            continue
-    word = ''.join(valid).lower()
     
-    if len(word) < 2:
-        return True
-    if word[0] != word[-1]:
-        return False
-    else:
-        return isPalindrome(word[1:-1])
+    
+    newS= [i.lower() for i in s if i.isalnum()]
+    return newS == newS[::-1]
 
 
 answer = isPalindrome("A man, a plan, a canal: Panama")
