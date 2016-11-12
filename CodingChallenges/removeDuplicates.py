74f390a40b16274681061864ee81b4279e25bbf7
PalashMatey <pm2824@columbia.edu>
2016-11-11 19:10:53 -0500

    RemoveDuplicates-Corner Case

--- CodingChallenges/removeDuplicates.py
+++ CodingChallenges/removeDuplicates.py
@@ -3,13 +3,15 @@ def removeDuplicates(nums):
         :type nums: List[int]
         :rtype: int
         """
+        if not nums:
+            return 0
         j = 0
         for i in range(1,len(nums)):
             if (nums[i] != nums[j]):
                 j += 1
                 nums[j] = nums[i]
 
-        print j + 1
+        return j + 1
                 
 
 
