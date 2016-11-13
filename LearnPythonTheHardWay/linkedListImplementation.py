ff2ce393fc569d70406c6a8d9aecab783736f02b
PalashMatey <pm2824@columbia.edu>
2016-11-13 18:17:23 -0500

    Singly Linked List
    Basic Linked List Implementation. Shall add more functions soon.

--- LearnPythonTheHardWay/linkedListImplementation.py
+++ LearnPythonTheHardWay/linkedListImplementation.py
@@ -18,6 +18,80 @@ class Node:
     def __str__(self):
         return str(self.data)
 
-temp = Node(23)
-print temp
+
+class UnorderedList:
+    
+    def __init__(self):
+        self.head = None
+    
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self,item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+    
+    def size(self):
+        count = 0
+        current = self.head
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+        return count
+
+    def search(self,item):
+        current = self.head
+        flag = False
+        while current != None and flag == False:
+            if (current.getData() == item):
+                flag = True
+            else:
+                current = current.getNext()
+        return flag
+
+    #Assuming that the element we are looking for is present in the list. 
+    
+    def remove(self,item):
+        current = self.head
+        previous = None
+        flag = False
+        while not flag:
+            if (current.getData() == item):
+                flag = True
+            else:
+                previous = current
+                current = current.getNext()
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+    
+    
+        
+
+
+mylist = UnorderedList()
+mylist.add(31)
+mylist.add(77)
+mylist.add(17)
+mylist.add(93)
+mylist.add(26)
+mylist.add(54)
+
+print(mylist.size())
+print(mylist.search(93))
+print(mylist.search(100))
+
+mylist.add(100)
+print(mylist.search(100))
+print(mylist.size())
+
+mylist.remove(54)
+print(mylist.size())
+mylist.remove(93)
+print(mylist.size())
+mylist.remove(31)
+print(mylist.size())
+print(mylist.search(93))
         
