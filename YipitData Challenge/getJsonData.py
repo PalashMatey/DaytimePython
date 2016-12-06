b6f24ef8e60e71e18f73c23829687fad679538b1
PalashMatey <pm2824@columbia.edu>
2016-12-06 17:07:55 -0500

    Able to extract data elements

--- YipitData Challenge/getJsonData.py
+++ YipitData Challenge/getJsonData.py
@@ -1,7 +1,19 @@
 import json, urllib
 from pprint import pprint
 
-url = "http://oscars.yipitdata.com/"
-response = urllib.urlopen(url)
-data = json.loads(response.read())
+def collect_data():
+    url = "http://oscars.yipitdata.com/"
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+    return data
+
+#Get all the names --> As a test
+def parse_data(data_collected):
+    movieNameList = []
+    names = data_collected['results'][0]['films'][0]['Detail URL']
+    print names
+
+collected_data = collect_data()
+parse_data(collected_data)
+    
 
