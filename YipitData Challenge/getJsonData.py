fef1f0ebde00daa010475ef981b702fddf2129b7
PalashMatey <pm2824@columbia.edu>
2016-12-06 21:42:07 -0500

    Conversion to millions and Float Array obtained
    Have to find the average and think about what to do for missing data

--- YipitData Challenge/getJsonData.py
+++ YipitData Challenge/getJsonData.py
@@ -14,7 +14,7 @@ def parse_data(data_collected):
     tableData = []
     movieBudget = []
     lengthOfResults =len(data_collected['results'])
-
+    print 'Year \t\t  Title \t\t Budget'
     for i in range(0,lengthOfResults):
         dataList = []
         detailedUrl = data_collected['results'][i]['films'][0]['Detail URL']
@@ -26,17 +26,21 @@ def parse_data(data_collected):
             movieBudget.append(convert_budget(budget))
 
         except KeyError:
-            budget = '$4 million'
+            budget = "US$1 million"
             movieBudget.append(convert_budget(budget))
-        dataList.append(year)
-        dataList.append(title)
-        dataList.append(budget)
-        tableData.append(dataList)
-        #print 'year -- title -- Budget', year,title,budget
-   # for row in tableData:
-    #    print("{: >25} {: >25} {: >25}".format(*row))
+        # dataList.append(year)
+        # dataList.append(title)
+        # dataList.append(budget)
+        # tableData.append(dataList)
+        print ' %s \t\t %s \t\t %s ' %(year.strip(''),title.lstrip(''),budget.lstrip())
+    # for row in tableData:
+    #     try:
+    #         print("{: >25} {: >25} {: >25}".format(*row))
+    #     except UnicodeDecodeError:
+    #         print row[0],row[1],row[2]
 
-    print 'Movie budgets are', movieBudget
+    
+    print 'Movie Budgets are',movieBudget
     #print 'Length of MovieBudget array is',len(movieBudget)
 
 def convert_budget(budget):
@@ -47,7 +51,15 @@ def convert_budget(budget):
     #else:
     #    winMovieBudget = re.findall(r"\u00a3([1-9]?[0-9,|.]+)",budget)
     #    print 'Pound actually got executed',winMovieBudget
-    return winMovieBudget[0],budget
+    
+    if ',' in winMovieBudget[0]:
+        winMovieBudget = winMovieBudget[0].replace(',','')
+        return float(winMovieBudget)
+    if '.' in winMovieBudget[0]:
+        winMovieBudget = float(winMovieBudget[0]) * 1000000
+        return winMovieBudget
+    else:
+        return float(winMovieBudget[0]) * 1000000
 
 
 
