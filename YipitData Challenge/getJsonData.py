31eb7febd6b5203a47153720341a650b8e56391b
PalashMatey <pm2824@columbia.edu>
2016-12-07 02:17:21 -0500

    Completed application

--- YipitData Challenge/getJsonData.py
+++ YipitData Challenge/getJsonData.py
@@ -1,8 +1,8 @@
 #!/usr/local/bin/python
 # -- coding: utf-8 --
 import json, urllib
-from pprint import pprint
 import re
+
 def collect_data(url):
 
     response = urllib.urlopen(url)
@@ -14,34 +14,36 @@ def parse_data(data_collected):
     tableData = []
     movieBudget = []
     lengthOfResults =len(data_collected['results'])
-    #print '\t\t\t\t   Year \t\t\t\t\tTitle \t\t\t\t Budget'
     dataList = ['Year','Title','Budget']
     tableData.append(dataList)
+    
     for i in range(0,lengthOfResults):
         dataList = []
         detailedUrl = data_collected['results'][i]['films'][0]['Detail URL']
         year = data_collected['results'][i]['year']
-        title = data_collected['results'][i]['films'][0]['Film']
+        title = collect_data(detailedUrl)
+        title = title['Title']
+
         try:
             budget = collect_data(detailedUrl)
             budget = budget['Budget']
             budget = convert_budget(budget)
             movieBudget.append(budget)
-
         except KeyError:
-            budget = "US$1 million"
+            budget = "US$0 million"
             budget = convert_budget(budget)
             movieBudget.append(budget)
+
         dataList.append(year.strip()[:4])
         dataList.append(title.strip())
-        dataList.append('$' + str(budget).strip())
+        if budget == 0:
+            dataList.append("Not Available")
+        dataList.append('$ ' + str(budget).strip())
         tableData.append(dataList)
-        #print 'year -- title -- Budget', year,title,budget
+
     for row in tableData:
-        print("{: >43} {: >43} {: >43}".format(*row))
-        #print ' %s \t\t %s \t\t\t\t\t\t $%s ' %(year.strip()[:4],title.strip(),str(budget).strip())
-   
-    print 'Movie Budgets are',movieBudget
+        print("{: >48} {: >48} {: >48}".format(*row))
+
     find_average(movieBudget)
     
 
@@ -57,10 +59,16 @@ def convert_budget(budget):
     
 
 def find_average(movieBudgets):
-
+    for num in range(0,len(movieBudgets)):
+        if movieBudgets[num] == 0:
+            try:
+                movieBudgets[num] = (movieBudgets[num-1] + movieBudgets [num + 1]) / 2
+            except:
+                pass
+    
     avg = sum(movieBudgets)/float(len(movieBudgets))
-    print 'The average budget of these movies is', avg
-    print 'In millions the average budget is ', avg / 1000000
+    print 'The average budget of these movies is $%.2f'%(avg)
+    
 
 def process_budget(winMovieBudget , flag):
     if flag == True:
@@ -73,10 +81,11 @@ def process_budget(winMovieBudget , flag):
             return winMovieBudget
         else:
             return float(winMovieBudget) * 1000000
+    
     else:
+        
         if ',' in winMovieBudget:
             winMovieBudget = winMovieBudget.replace(',','')
-            
             return float(winMovieBudget) * 1.27
         if '.' in winMovieBudget:
             winMovieBudget = float(winMovieBudget) * 1000000 *1.27
