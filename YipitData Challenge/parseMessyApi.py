43c49f75b99fc5e9634ddc6bbb8516826e5f682d
PalashMatey <pm2824@columbia.edu>
2016-12-07 02:38:40 -0500

    Properly Commented Code

--- YipitData Challenge/parseMessyApi.py
+++ YipitData Challenge/parseMessyApi.py
@@ -4,41 +4,48 @@ import json, urllib
 import re
 
 def collect_data(url):
-
+    #Function to collect data from given Url
     response = urllib.urlopen(url)
     data = json.loads(response.read())
     return data
 
 def parse_data(data_collected):
-    
+    #Main function where each element is parsed
     tableData = []
     movieBudget = []
     lengthOfResults =len(data_collected['results'])
     dataList = ['Year','Title','Budget']
     tableData.append(dataList)
     
+    #running through to get details of all the winners
     for i in range(0,lengthOfResults):
         dataList = []
         detailedUrl = data_collected['results'][i]['films'][0]['Detail URL']
         year = data_collected['results'][i]['year']
+        #title present in both 'film' and 'Detail Url' --> using 'Detail Url'' to get 'Title'(Although it takes longer)
+        #
         title = collect_data(detailedUrl)
         title = title['Title']
 
+        #For some Winners --> Budget Field is missing
+        #convert_budget --> parses the Budget Field --> returning Float
         try:
             budget = collect_data(detailedUrl)
             budget = budget['Budget']
             budget = convert_budget(budget)
             movieBudget.append(budget)
         except KeyError:
+            #Placeholder Budget Value --> Makes parsing easier --> Value replaced with Mean during Average Calculation
             budget = "US$0 million"
             budget = convert_budget(budget)
             movieBudget.append(budget)
 
         dataList.append(year.strip()[:4])
         dataList.append(title.strip())
+        #Ensure data is correctly displayed in the table when Not Available 
         if budget == 0:
             dataList.append("Not Available")
-        dataList.append('$ ' + str(budget).strip())
+        dataList.append('$' + str(budget).strip())
         tableData.append(dataList)
 
     for row in tableData:
@@ -49,7 +56,8 @@ def parse_data(data_collected):
 
 def convert_budget(budget):
     budget = budget.strip()
-    
+    #There are more than one currencies in the data --> Considering just Dollar and Pound
+    #process_budget returns converted value in Dollar
     if '$' in budget:
         winMovieBudget = re.findall(r"\$?([0-9]?[0-9,|.]+)",budget)
         return process_budget(winMovieBudget[0], True)
@@ -57,19 +65,6 @@ def convert_budget(budget):
         winMovieBudget = re.findall(r"\$?([0-9]?[0-9,|.]+)",budget)
         return process_budget(winMovieBudget[0],False)
     
-
-def find_average(movieBudgets):
-    for num in range(0,len(movieBudgets)):
-        if movieBudgets[num] == 0:
-            try:
-                movieBudgets[num] = (movieBudgets[num-1] + movieBudgets [num + 1]) / 2
-            except:
-                pass
-    
-    avg = sum(movieBudgets)/float(len(movieBudgets))
-    print 'The average budget of these movies is $%.2f'%(avg)
-    
-
 def process_budget(winMovieBudget , flag):
     if flag == True:
     
@@ -83,7 +78,7 @@ def process_budget(winMovieBudget , flag):
             return float(winMovieBudget) * 1000000
     
     else:
-        
+        #conversion rate for pound -> Dollar is currently at 1.27
         if ',' in winMovieBudget:
             winMovieBudget = winMovieBudget.replace(',','')
             return float(winMovieBudget) * 1.27
@@ -92,6 +87,19 @@ def process_budget(winMovieBudget , flag):
             return winMovieBudget
         else:
             return float(winMovieBudget) * 1000000 * 1.27
+
+def find_average(movieBudgets):
+    #Taking the mean for those Budget Values which are not available 
+    for num in range(0,len(movieBudgets)):
+        if movieBudgets[num] == 0:
+            try:
+                movieBudgets[num] = (movieBudgets[num-1] + movieBudgets [num + 1]) / 2
+            except:
+                pass
+    
+    avg = sum(movieBudgets)/float(len(movieBudgets))
+    print 'The average budget of these movies is $%.2f'%(avg)
+    
 
 
 url = "http://oscars.yipitdata.com/"
