3584bda970d565e4b5b7bee4943a3e19b0195847
PalashMatey <pm2824@columbia.edu>
2016-12-07 01:01:24 -0500

    Table Printed
    Only thing left is to be able to handle missing data

--- YipitData Challenge/getJsonData.py
+++ YipitData Challenge/getJsonData.py
@@ -14,7 +14,9 @@ def parse_data(data_collected):
     tableData = []
     movieBudget = []
     lengthOfResults =len(data_collected['results'])
-    print 'Year \t\t  Title \t\t\t\t\t Budget'
+    #print '\t\t\t\t   Year \t\t\t\t\tTitle \t\t\t\t Budget'
+    dataList = ['Year','Title','Budget']
+    tableData.append(dataList)
     for i in range(0,lengthOfResults):
         dataList = []
         detailedUrl = data_collected['results'][i]['films'][0]['Detail URL']
@@ -30,7 +32,14 @@ def parse_data(data_collected):
             budget = "US$1 million"
             budget = convert_budget(budget)
             movieBudget.append(budget)
-        print ' %s \t\t %s \t\t\t\t\t\t $%s ' %(year.strip()[:4],title.strip(),str(budget).strip())
+        dataList.append(year.strip()[:4])
+        dataList.append(title.strip())
+        dataList.append('$' + str(budget).strip())
+        tableData.append(dataList)
+        #print 'year -- title -- Budget', year,title,budget
+    for row in tableData:
+        print("{: >43} {: >43} {: >43}".format(*row))
+        #print ' %s \t\t %s \t\t\t\t\t\t $%s ' %(year.strip()[:4],title.strip(),str(budget).strip())
    
     print 'Movie Budgets are',movieBudget
     find_average(movieBudget)
