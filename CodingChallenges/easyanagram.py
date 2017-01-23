616b8ef10535961e5cd5f8a8f1df14d231220aa5
PalashMatey <pm2824@columbia.edu>
2017-01-23 01:22:25 -0500

    Updated IsAnagram Program
    The use of dict.get() is awesome! Shortened the implementation by a
    mile!

--- CodingChallenges/easyanagram.py
+++ CodingChallenges/easyanagram.py
@@ -4,29 +4,36 @@ def isAnagram( s, t):
         :type t: str
         :rtype: bool
         """
-        dictStore = {}
-        dictStore2 = {}
-        for characters in s:
-            if characters in dictStore:
-                dictStore[characters] += 1
-            else:
-                dictStore[characters] = 1
-        for charaters2 in t:
-            if(charaters2 in dictStore):
-                dictStore[charaters2] -= 1
 
-        for characters2 in t:
-            if characters2 in dictStore2:
-                dictStore2[characters2] += 1
-            else:
-                dictStore2[characters2] = 1 
-        for v in dictStore.values():
-            if (v>0):
-                return False
-        for v in dictStore2.values():
-            if (v > 0):
-                return False
-        return True
+        dic1,dic2 = {},{}
+        for item in s:
+            dic1[item] = dic1.get(item,0) + 1
+        for item in t:
+            dic2[item] = dic2.get(item,0) + 1
+        return dic1 == dic2
+        # dictStore = {}
+        # dictStore2 = {}
+        # for characters in s:
+        #     if characters in dictStore:
+        #         dictStore[characters] += 1
+        #     else:
+        #         dictStore[characters] = 1
+        # for charaters2 in t:
+        #     if(charaters2 in dictStore):
+        #         dictStore[charaters2] -= 1
 
-answer = isAnagram("a","ab")
+        # for characters2 in t:
+        #     if characters2 in dictStore2:
+        #         dictStore2[characters2] += 1
+        #     else:
+        #         dictStore2[characters2] = 1 
+        # for v in dictStore.values():
+        #     if (v>0):
+        #         return False
+        # for v in dictStore2.values():
+        #     if (v > 0):
+        #         return False
+        # return True
+
+answer = isAnagram("anagram","nagaram")
 print answer
