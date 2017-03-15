377259fd59e0b430f3f8011defa9b57840eafbce
PalashMatey <pm2824@columbia.edu>
2017-03-14 20:34:52 -0400

    Lowest Common Ancestor of a Binary Tree

---
+++ CodingChallenges/lowestCommonAncestor.py
@@ -0,0 +1,23 @@
+#For a generic Binary tree 
+
+def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        if root == p or root == q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p , q)
+        
+        if not left and not right:
+            return None
+        if left and right:
+            return root
+        node = left if left != None else right
+        return node
